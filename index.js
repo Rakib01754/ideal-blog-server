adcba630df99b305e11801a9103e97421eda187e
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-12-28 12:26:40 +0600

    mongodb connection succes with dotenv config

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
+require('dotenv').config()
+
 
 app.use(cors())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.n58ahyf.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const serviceCollection = client.db("packersService").collection("services");
+        const reviewCollection = client.db("packersService").collection("reviews");
+
+
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(err => console.log(err));
+
 
 app.get('/', (req, res) => {
     res.send('Blog server running')
